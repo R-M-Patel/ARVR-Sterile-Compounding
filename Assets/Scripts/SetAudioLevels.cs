c5d4e5c5922c1a02a1a912d3bfb2c136050112b3
Brennan Bayne <bmb101@pitt.edu>
2017-03-31 05:14:56 -0400

    Adding demo for final coding sprint

---
+++ Assets/Scripts/SetAudioLevels.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetAudioLevels : MonoBehaviour {
+
+	public AudioMixer MasterMixer;                    //Used to hold a reference to the AudioMixer mainMixer
+
+
+    //Call this function and pass in the float parameter masterLvl to set the volume of the AudioMixerGroup Master in mainMixer
+    public void SetMasterLevel(float masterLvl)
+    {
+        MasterMixer.SetFloat("masterVol", masterLvl);
+    }
+
+    //Call this function and pass in the float parameter menuLvl to set the volume of the AudioMixerGroup MenuSounds in mainMixer
+    public void SetMenuSoundsLevel(float menuLvl)
+	{
+        MasterMixer.SetFloat("menuVol", menuLvl);
+	}
+
+	//Call this function and pass in the float parameter sfxLevel to set the volume of the AudioMixerGroup InGameSoundFx in mainMixer
+	public void SetSfxLevel(float sfxLevel)
+	{
+        MasterMixer.SetFloat("sfxVol", sfxLevel);
+	}
+}
